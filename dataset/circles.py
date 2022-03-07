3f5cda01fcf24869e5359122d51c5a020a072e1a
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-03-07 18:08:26 +0000

    Added circle precision

--- dataset/circles.py
+++ dataset/circles.py
@@ -1,23 +1,25 @@
 import math
 import time
-from decimal import Decimal
+from decimal import Decimal, getcontext
 from typing import List, Generator, Tuple
 from functools import cached_property
+
 
 class Circle:
     diameter: Decimal
 
-    def __init__(self, diameter: float) -> None:
+    def __init__(self, diameter: float, prec: int = 100) -> None:
         self.diameter = Decimal(diameter)
+        getcontext().prec = prec
 
     def __str__(self) -> str:
-        return "\n".join(map(lambda x: " ".join(map(str, x)), self.fill()))
+        return "\n".join(map(lambda x: " ".join(map(lambda x: "#" if x else " ", x)), self.fill()))
 
     def fill(self) -> List[List[int]]:
         arr, size = self.blank_output_array()
         h, k = self.center(size)
-        for i in range(size):
-            for j in range(size):
+        for j in range(size):
+            for i in range(size):
                 if self.logic(i, j, h, k):
                     arr[j][i] = 1
         return arr
@@ -35,18 +37,4 @@ class Circle:
 
     def center(self, size: int) -> Tuple[int, int]:
         return (math.ceil(size / 2),) * 2
-
-
-if __name__ == "__main__":
-    for i in range(2 * 100, 20 * 100):
-        circle = Circle(i / 100)
-        print("--------------")
-        print(circle)
-        print("--------------")
-        time.sleep(0.001)
-        print(chr(27) + "[2J")
-    circle = Circle(i / 100)
-    print("--------------")
-    print(circle)
-    print("--------------")
     
